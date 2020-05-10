880264697a23e6fe9578bf605ac60fccff494903
arguetaf <ferda2212@hotmail.com>
2020-05-10 17:43:17 -0500

    Modulo crear usuario

--- routes/administrador.js
+++ routes/administrador.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const passport = require('passport');
 const sql = require('../helpers/databaseManager');
 
+let alertaModulos=false;
+let mensajeAlertaModulos="";
+
 router.get('/inicio', function(req,res,next)
 {
     res.render('Administrador/inicio', {administrador:req.user})
@@ -11,9 +14,39 @@ router.get('/inicio', function(req,res,next)
 
 router.get('/usuarios', function(req,res,next)
 {
-    res.render('Administrador/usuarios', {administrador:req.user})
+    res.render('Administrador/usuarios', {administrador:req.user, alertaModulos:alertaModulos, mensajeAlertaModulos:mensajeAlertaModulos})
+    alertaModulos=false;
+    mensajeAlertaModulos="";
+});
+
+router.get('/usuarios/crear', function(req,res,next)
+{
+    res.render('Administrador/crear-usuario', {administrador:req.user})
 
 });
+
+router.post('/usuarios/crear', function(req,res,next)
+{
+    console.log(req.body);
+    var query="insert into usuario (NOMBRE, APELLIDO_P, APELLIDO_M, EMAIL, CONTRASENIA, ADMINISTRADOR) " +
+    "values ('" + req.body.nombre + "', '" + req.body.apellidoPaterno + "', '" + req.body.apellidoMaterno + "'," +
+    "'" + req.body.email + "', '" + req.body.password + "', 'false')";
+
+    sql.query(query, (respuestaQuery)=>
+    {
+        if(respuestaQuery.rowsAffected > 0)
+        {
+            alertaModulos=true;
+            mensajeAlertaModulos="Usuario Creado";
+            res.redirect('./');
+        }
+        
+    })
+
+
+});
+
+
 
 router.get('/productos', function(req,res,next)
 {
